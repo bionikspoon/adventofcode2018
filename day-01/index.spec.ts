996067d1b0cbfb2625b69b548379767a58259c6b
Manu Phatak <manu@thinkcerca.com>
2018-12-01 17:13:14 -0600

    [day-01-part-1] draft

--- day-01/index.spec.ts
+++ day-01/index.spec.ts
@@ -1,7 +1,32 @@
-import greeter from '.'
+import chronalCalibration from '.'
+import fs from 'fs'
+import path from 'path'
 
-test('it says hello', () => {
-  const user = 'Jane User'
+const example1 = `
++1
++1
++1
+`
+const example2 = `
++1
++1
+-2
+`
 
-  expect(greeter(user)).toEqual('Hello, Jane User')
+const example3 = `
+-1
+-2
+-3
+`
+
+const example4 = fs.readFileSync(path.join(__dirname, 'input.txt')).toString()
+
+test.each`
+  case | input       | expected
+  ${1} | ${example1} | ${3}
+  ${2} | ${example2} | ${0}
+  ${3} | ${example3} | ${-6}
+  ${4} | ${example4} | ${585}
+`('case $case', ({ input, expected }) => {
+  expect(chronalCalibration(input)).toEqual(expected)
 })
--- day-01/index.ts
+++ day-01/index.ts
@@ -1,3 +1,9 @@
-export default function greeter(person: string) {
-  return `Hello, ${person}`
+export default function chronalCalibration(input: string) {
+  const operation = input
+    .trim()
+    .split('\n')
+    .map(line => line.trim())
+    .join(' ')
+
+  return eval(`0 ${operation}`)
 }
