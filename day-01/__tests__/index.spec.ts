dd27ab39e72ea893b44340e089e2f7d2111ee11b
Manu Phatak <manu@thinkcerca.com>
2018-12-02 11:21:02 -0600

    [day-01-part-2] use a binary tree to improve speed

--- day-01/__tests__/index.spec.ts
+++ day-01/__tests__/index.spec.ts
@@ -1,7 +1,12 @@
-import { chronalCalibrationSum, chronalCalibrationRepeat, cycle } from '..'
 import fs from 'fs'
 import path from 'path'
 import { promisify } from 'util'
+import {
+  BinaryTree,
+  chronalCalibrationRepeat,
+  chronalCalibrationSum,
+  cycle,
+} from '..'
 
 describe('#chronalCalibrationSum', () => {
   test.each`
@@ -42,6 +47,45 @@ describe('#cycle', () => {
     const generator = cycle([1, 2, 3])
 
     expect(take(1, generator)).toEqual([1])
+  })
+})
+
+describe('#BinaryTree', () => {
+  test('it has an initial value', () => {
+    const tree = new BinaryTree(0)
+
+    expect(tree.value).toEqual(0)
+  })
+
+  test('it can insert numbers', () => {
+    const tree = new BinaryTree(0)
+
+    tree.insert(1)
+
+    expect(tree.toArray()).toEqual([0, 1])
+  })
+
+  test('it maintains order', () => {
+    const tree = new BinaryTree(5)
+
+    tree
+      .insert(3)
+      .insert(7)
+      .insert(8)
+      .insert(2)
+      .insert(4)
+
+    expect(tree.toArray()).toEqual([2, 3, 4, 5, 7, 8])
+  })
+
+  test('it throws if you try to add duplicates', () => {
+    const tree = new BinaryTree(5)
+
+    tree.insert(2).insert(6)
+
+    expect(() => {
+      tree.insert(6)
+    }).toThrowError('BinaryTree.value already exists: 6')
   })
 })
 
--- day-01/index.ts
+++ day-01/index.ts
@@ -12,19 +12,25 @@ export function chronalCalibrationRepeat(input: string, maxSize = 1000000) {
   const partialsGenerator = cycle(partials)
 
   let state = 0
-  let appenders: number[] = [state]
   let i = 0
+  const tree = new BinaryTree(state)
 
   for (const partial of partialsGenerator) {
-    if (i >= maxSize)
+    if (i >= maxSize) {
       throw new Error(`Potential infinite loop, increase maxSize: ${maxSize}`)
-    if (!partial)
+    }
+    if (!partial) {
       throw new Error(`Partial is undefined in partials: ${partials}`)
+    }
 
     state = partial(state)
 
-    if (appenders.includes(state)) return state
-    appenders.push(state)
+    try {
+      tree.insert(state)
+    } catch (error) {
+      return state
+    }
+
     i++
   }
 }
@@ -47,9 +53,11 @@ const parseInput = (input: string) =>
     .map(line => line.trim())
     .map(partiallyApplyLine)
 
-export function* cycle<T>(items: Array<T>) {
-  if (items.length <= 0)
+export function* cycle<T>(items: T[]) {
+  if (items.length <= 0) {
     throw new Error('Cycle(items): items must have length > 0')
+  }
+
   let i = 0
 
   while (true) {
@@ -60,5 +68,53 @@ export function* cycle<T>(items: Array<T>) {
     } else {
       i++
     }
+  }
+}
+
+export class BinaryTree<T> {
+  public value: T
+  private left: BinaryTree<T> | null
+  private right: BinaryTree<T> | null
+
+  constructor(value: T) {
+    this.value = value
+    this.left = null
+    this.right = null
+  }
+
+  public insert(value: T) {
+    if (value === this.value) {
+      throw new Error(`BinaryTree.value already exists: ${value}`)
+    }
+
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = new BinaryTree(value)
+      } else {
+        this.left.insert(value)
+      }
+    }
+
+    if (value > this.value) {
+      if (this.right === null) {
+        this.right = new BinaryTree(value)
+      } else {
+        this.right.insert(value)
+      }
+    }
+
+    return this
+  }
+
+  public toArray() {
+    return Array.from(this.values())
+  }
+
+  private *values(): any {
+    if (this.left !== null) yield* this.left.values()
+
+    yield this.value
+
+    if (this.right !== null) yield* this.right.values()
   }
 }
