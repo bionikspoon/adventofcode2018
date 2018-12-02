a3e066fdcb08a33b1d982f1b423710cb300e4f06
Manu Phatak <manu@thinkcerca.com>
2018-12-02 09:34:42 -0600

    [day-01-part-2] refactor while loop into partial generator

--- day-01/__tests__/index.spec.ts
+++ day-01/__tests__/index.spec.ts
@@ -1,7 +1,49 @@
-import { chronalCalibration1, chronalCalibration2 } from '..'
+import { chronalCalibrationSum, chronalCalibrationRepeat, cycle } from '..'
 import fs from 'fs'
 import path from 'path'
 import { promisify } from 'util'
+
+describe('#chronalCalibrationSum', () => {
+  test.each`
+    file                   | expected
+    ${'part-1-case-1.txt'} | ${3}
+    ${'part-1-case-2.txt'} | ${0}
+    ${'part-1-case-3.txt'} | ${-6}
+    ${'input.txt'}         | ${585}
+  `('case $file is reduced to $expected', async ({ expected, file }) => {
+    const input = await getInput(file)
+
+    expect(chronalCalibrationSum(input.toString())).toEqual(expected)
+  })
+})
+
+describe('#chronalCalibrationSum', () => {
+  test.each`
+    file                   | expected
+    ${'part-2-case-1.txt'} | ${0}
+    ${'part-2-case-2.txt'} | ${10}
+    ${'part-2-case-3.txt'} | ${5}
+    ${'part-2-case-4.txt'} | ${14}
+    ${'input.txt'}         | ${83173}
+  `('case $file is reduced to $expected', async ({ expected, file }) => {
+    const input = await getInput(file)
+
+    expect(chronalCalibrationRepeat(input)).toEqual(expected)
+  })
+})
+
+describe('#cycle', () => {
+  test('it cycles an array', () => {
+    const generator = cycle([1, 2, 3])
+
+    expect(take(5, generator)).toEqual([1, 2, 3, 1, 2])
+  })
+  test('it cycles an array', () => {
+    const generator = cycle([1, 2, 3])
+
+    expect(take(1, generator)).toEqual([1])
+  })
+})
 
 const readFileAsync = promisify(fs.readFile)
 
@@ -10,27 +52,14 @@ const getInput = (file: string) =>
     buffer.toString()
   )
 
-test.each`
-  file       | expected
-  ${'1.txt'} | ${3}
-  ${'2.txt'} | ${0}
-  ${'3.txt'} | ${-6}
-  ${'4.txt'} | ${585}
-`('case $file is reduced to $expected', async ({ expected, file }) => {
-  const input = await getInput(file)
+const take = <T>(n: number, gen: IterableIterator<T>) => {
+  const results = []
 
-  expect(chronalCalibration1(input.toString())).toEqual(expected)
-})
+  for (const value of gen) {
+    results.push(value)
 
-test.each`
-  file                   | expected
-  ${'part-2-case-1.txt'} | ${1}
-  ${'part-2-case-2.txt'} | ${10}
-  ${'part-2-case-3.txt'} | ${5}
-  ${'part-2-case-4.txt'} | ${14}
-  ${'4.txt'}             | ${83173}
-`('case $file is reduced to $expected', async ({ expected, file }) => {
-  const input = await getInput(file)
+    if (results.length >= n) break
+  }
 
-  expect(chronalCalibration2(input)).toEqual(expected)
-})
+  return results
+}
--- day-01/index.ts
+++ day-01/index.ts
@@ -1,32 +1,31 @@
 import R from 'ramda'
 
-export function chronalCalibration1(input: string) {
+export function chronalCalibrationSum(input: string) {
   return parseInput(input).reduce(
     (acc, fn) => (fn === undefined ? acc : fn(acc)),
     0
   )
 }
 
-export function chronalCalibration2(input: string) {
+export function chronalCalibrationRepeat(input: string, maxSize = 1000000) {
   const partials = parseInput(input)
+  const partialsGenerator = cycle(partials)
 
-  const appenders: any[] = []
-  let i = 0
   let state = 0
-  while (true) {
-    const fn = partials[i]
-    if (!fn) throw new Error(`No partials[${i}] for partials: ${partials}`)
-    state = fn(state)
+  let appenders: number[] = [state]
+  let i = 0
+
+  for (const partial of partialsGenerator) {
+    if (i >= maxSize)
+      throw new Error(`Potential infinite loop, increase maxSize: ${maxSize}`)
+    if (!partial)
+      throw new Error(`Partial is undefined in partials: ${partials}`)
+
+    state = partial(state)
 
     if (appenders.includes(state)) return state
-
     appenders.push(state)
-
-    if (i >= partials.length - 1) {
-      i = 0
-    } else {
-      i++
-    }
+    i++
   }
 }
 
@@ -47,3 +46,19 @@ const parseInput = (input: string) =>
     .split('\n')
     .map(line => line.trim())
     .map(partiallyApplyLine)
+
+export function* cycle<T>(items: Array<T>) {
+  if (items.length <= 0)
+    throw new Error('Cycle(items): items must have length > 0')
+  let i = 0
+
+  while (true) {
+    yield items[i]
+
+    if (i >= items.length - 1) {
+      i = 0
+    } else {
+      i++
+    }
+  }
+}
