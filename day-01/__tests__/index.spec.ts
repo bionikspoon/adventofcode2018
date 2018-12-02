b7b3dbb6eb9f18c66832bfa9dacfc5c2c2d3be98
Manu Phatak <manu@thinkcerca.com>
2018-12-01 23:15:37 -0600

    [day-01-part-2] first draft

--- day-01/__tests__/index.spec.ts
+++ day-01/__tests__/index.spec.ts
@@ -1,9 +1,14 @@
-import chronalCalibration from '..'
+import { chronalCalibration1, chronalCalibration2 } from '..'
 import fs from 'fs'
 import path from 'path'
 import { promisify } from 'util'
 
 const readFileAsync = promisify(fs.readFile)
+
+const getInput = (file: string) =>
+  readFileAsync(path.join(__dirname, 'case', file)).then(buffer =>
+    buffer.toString()
+  )
 
 test.each`
   file       | expected
@@ -12,7 +17,20 @@ test.each`
   ${'3.txt'} | ${-6}
   ${'4.txt'} | ${585}
 `('case $file is reduced to $expected', async ({ expected, file }) => {
-  const input = await readFileAsync(path.join(__dirname, 'case', file))
+  const input = await getInput(file)
 
-  expect(chronalCalibration(input.toString())).toEqual(expected)
+  expect(chronalCalibration1(input.toString())).toEqual(expected)
+})
+
+test.each`
+  file                   | expected
+  ${'part-2-case-1.txt'} | ${0}
+  ${'part-2-case-2.txt'} | ${10}
+  ${'part-2-case-3.txt'} | ${5}
+  ${'part-2-case-4.txt'} | ${14}
+  ${'4.txt'}             | ${83173}
+`('case $file is reduced to $expected', async ({ expected, file }) => {
+  const input = await getInput(file)
+
+  expect(chronalCalibration2(input)).toEqual(expected)
 })
--- day-01/index.ts
+++ day-01/index.ts
@@ -1,5 +1,35 @@
 import R from 'ramda'
 
+export function chronalCalibration1(input: string) {
+  return parseInput(input).reduce(
+    (acc, fn) => (fn === undefined ? acc : fn(acc)),
+    0
+  )
+}
+
+export function chronalCalibration2(input: string) {
+  const partials = parseInput(input)
+
+  const appenders: any[] = []
+  let i = 0
+  let state = 0
+  while (true) {
+    const fn = partials[i]
+    if (!fn) throw new Error(`No partials[${i}] for partials: ${partials}`)
+    state = fn(state)
+
+    if (appenders.includes(state)) return state
+
+    appenders.push(state)
+
+    if (i >= partials.length - 1) {
+      i = 0
+    } else {
+      i++
+    }
+  }
+}
+
 const RE_MATCH_LINE = new RegExp(/^(?<op>\+|-)(?<value>\d+)$/, 'um')
 const partiallyApplyLine = (line: string) => {
   const match = RE_MATCH_LINE.exec(line)
@@ -11,11 +41,9 @@ const partiallyApplyLine = (line: string) => {
   return (n: number) => opFn(n, value)
 }
 
-export default function chronalCalibration(input: string) {
-  return input
+const parseInput = (input: string) =>
+  input
     .trim()
     .split('\n')
     .map(line => line.trim())
     .map(partiallyApplyLine)
-    .reduce((acc, fn) => (fn === undefined ? acc : fn(acc)), 0)
-}
