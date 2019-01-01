e8febc9d9983f7eb5349638640b0c3bcb230251b
Manu Phatak <manu@thinkcerca.com>
2019-01-01 15:04:03 -0600

    solve part 1

--- src/day-18/Acre.ts
+++ src/day-18/Acre.ts
@@ -1,44 +1,80 @@
 import { Token } from './shared'
 
-export function acreFromToken(x: number, y: number, token: Token) {
-  switch (token) {
-    case Token.OPEN:
-      return new OpenAcre(x, y)
-    case Token.TREE:
-      return new TreeAcre(x, y)
-    case Token.LUMBERYARD:
-      return new LumberyardAcre(x, y)
-  }
-}
-
-export abstract class Acre {
+export class Acre {
   public static getKey(x: number, y: number) {
     return [y, x].map(n => n.toString().padStart(2, '0')).join('_')
   }
   public readonly x: number
   public readonly y: number
-  public abstract readonly token: Token
+  public value: AcreValue
   private readonly key: string
 
-  constructor(x: number, y: number) {
+  constructor(x: number, y: number, token: Token) {
     this.x = x
     this.y = y
 
     this.key = Acre.getKey(x, y)
+    this.value = acreValueFromToken(token)
   }
 
   public toString() {
     return this.key
   }
+
+  public evolve(neighbors: AcreValue[]) {
+    const nextValue = this.value.willBecome(neighbors)
+    this.value = nextValue
+    return this
+  }
 }
 
-class OpenAcre extends Acre {
-  public token: Token = Token.OPEN
+function acreValueFromToken(token: Token) {
+  switch (token) {
+    case Token.OPEN:
+      return new OpenAcre()
+    case Token.TREE:
+      return new TreeAcre()
+    case Token.LUMBERYARD:
+      return new LumberyardAcre()
+  }
 }
 
-class TreeAcre extends Acre {
-  public token: Token = Token.TREE
+export abstract class AcreValue {
+  public abstract readonly token: Token
+  public abstract willBecome(neighbors: AcreValue[]): AcreValue
 }
-class LumberyardAcre extends Acre {
-  public token: Token = Token.LUMBERYARD
+
+class OpenAcre extends AcreValue {
+  public readonly token: Token = Token.OPEN
+  public willBecome(neighbors: AcreValue[]): AcreValue {
+    const neighborsWithTrees = neighbors.filter(
+      neighbor => neighbor instanceof TreeAcre
+    )
+    return neighborsWithTrees.length >= 3 ? new TreeAcre() : this
+  }
+}
+
+class TreeAcre extends AcreValue {
+  public readonly token: Token = Token.TREE
+  public willBecome(neighbors: AcreValue[]): AcreValue {
+    const neighborsWithLumberyards = neighbors.filter(
+      neighbor => neighbor instanceof LumberyardAcre
+    )
+    return neighborsWithLumberyards.length >= 3 ? new LumberyardAcre() : this
+  }
+}
+class LumberyardAcre extends AcreValue {
+  public readonly token: Token = Token.LUMBERYARD
+  public willBecome(neighbors: AcreValue[]): AcreValue {
+    const neighborsWithLumberyards = neighbors.filter(
+      neighbor => neighbor instanceof LumberyardAcre
+    )
+    const neighborsWithTrees = neighbors.filter(
+      neighbor => neighbor instanceof TreeAcre
+    )
+    return neighborsWithTrees.length >= 1 &&
+      neighborsWithLumberyards.length >= 1
+      ? this
+      : new OpenAcre()
+  }
 }
--- src/day-18/Grid.ts
+++ src/day-18/Grid.ts
@@ -1,26 +1,78 @@
-import { groupBy, pipe, values } from 'ramda'
-import { Graph, GraphVertex } from '../utils/Graph'
-import { Acre, acreFromToken } from './Acre'
+import { groupBy, pipe, values, map, length } from 'ramda'
+import { Graph, GraphEdge, GraphVertex } from '../utils/Graph'
+import { Acre, AcreValue } from './Acre'
 import { Token } from './shared'
+
+const directions = [
+  [0, 1],
+  [1, 1],
+  [1, 0],
+  [1, -1],
+  [0, -1],
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+]
 
 export default class Grid extends Graph<Acre> {
   public static from(tokenList: Token[][]) {
     const grid = tokenList.reduce(
       (g, tokenRow, y) =>
         tokenRow.reduce((_, token, x) => {
-          const acre = acreFromToken(x, y, token)
+          const acre = new Acre(x, y, token)
           const vertex = new GraphVertex(acre)
           return g.addVertex(vertex)
         }, g),
-      new Grid()
+      new Grid(true)
     )
+
+    grid.getAllVertices().forEach(vertex => {
+      directions.forEach(([x, y]) => {
+        const neighborKey = Acre.getKey(vertex.value.x + x, vertex.value.y + y)
+        const neighbor = grid.getVertexByKey(neighborKey)
+        if (neighbor) grid.addEdge(new GraphEdge(vertex, neighbor))
+      })
+    })
     return grid
   }
 
   public print() {
     return groupByAcreY(this.getAllVertices())
-      .map(rows => rows.map(vertex => vertex.value.token).join(''))
+      .map(rows => rows.map(vertex => vertex.value.value.token).join(''))
       .join('\n')
+  }
+
+  public simulateMinutes(minutes: number) {
+    for (let i = 0; i < minutes; i++) {
+      this.getAllVertices()
+        .map(
+          vertex =>
+            [
+              vertex.value,
+              vertex
+                .getNeighbors()
+                .toArray()
+                .map(neighbor => neighbor.value.value),
+            ] as [Acre, AcreValue[]]
+        )
+        .forEach(([acre, neighbors]) => {
+          acre.evolve(neighbors)
+        })
+    }
+
+    return this
+  }
+
+  public stats() {
+    return mapLengths(
+      groupByToken(this.getAllVertices().map(vertex => vertex.value.value))
+    )
+  }
+
+  public answer() {
+    const stats = this.stats()
+
+    return stats[Token.TREE] * stats[Token.LUMBERYARD]
   }
 }
 
@@ -30,3 +82,11 @@ const groupByAcreY: (
   groupBy<GraphVertex<Acre>>(vertex => vertex.value.y.toString()),
   values
 )
+
+const groupByToken: (
+  values: AcreValue[]
+) => { [token: string]: AcreValue[] } = groupBy(value => value.token)
+
+const mapLengths: <T>(
+  groups: { [token: string]: T[] }
+) => { [token: string]: number } = map(length)
