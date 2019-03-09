5bd0b72ed8cb203e4c53bfaddf5eaaff2c148882
Manu Phatak <manu@thinkcerca.com>
2019-03-09 17:22:10 -0600

    solve part 2

--- lib/day-02/main.rb
+++ lib/day-02/main.rb
@@ -2,6 +2,7 @@
 
 module Day02
   module Part1
+    # @param id [String]
     def count_repeats(id)
       counter = Hash.new(0)
 
@@ -10,7 +11,9 @@ module Day02
       [counter.value?(2), counter.value?(3)]
     end
 
+    # @param file [File]
     def checksum(file)
+      # @type Array<String>
       repeats = file.each_line.map(&:strip).map { |id| count_repeats(id) }
 
       Results.new(
@@ -29,6 +32,28 @@ module Day02
         @two = two
         @three = three
         @checksum = two * three
+      end
+    end
+  end
+
+  module Part2
+    # @param left [String]
+    # @param right [String]
+    def find_simularities(left, right)
+      left
+        .each_char
+        .zip(right.each_char)
+        .map { |(l, r)| l if l == r }
+        .compact
+        .join
+    end
+
+    # @param file [File]
+    def find_common_chars(file)
+      combinations = file.each_line.map(&:strip).combination(2)
+      combinations.each do |(l, r)|
+        simularities = find_simularities(l, r)
+        break simularities if simularities.length == l.length - 1
       end
     end
   end
--- spec/day-02/main_spec.rb
+++ spec/day-02/main_spec.rb
@@ -8,8 +8,9 @@ describe Day02::Part1 do
   describe '#count_repeats' do
     def self.test_count_repeats(input, expected)
       context "given input '#{input}'" do
+        subject(:results) { count_repeats(input) }
         it 'counts repeat characters' do
-          expect(count_repeats(input)).to eq expected
+          expect(results).to eq expected
         end
       end
     end
@@ -26,10 +27,8 @@ describe Day02::Part1 do
   describe '#checksum' do
     def self.test_checksum(case_name, two, three, checksum)
       context "given input '#{case_name}'" do
-        subject(:results) do
-          input = with_input(case_name)
-          checksum(input)
-        end
+        let(:input) { with_input(case_name) }
+        subject(:results) { checksum(input) }
 
         it "has '#{two}' ids with 2 repeats" do
           expect(results.two).to eql two
@@ -45,6 +44,41 @@ describe Day02::Part1 do
 
     test_checksum('part-1-case-1.txt', 4, 3, 12)
     test_checksum('input.txt', 250, 24, 6000)
+  end
+end
+
+describe Day02::Part2 do
+  include Day02::Part2
+
+  describe 'find_simularities' do
+    def self.test_find_simularities(input, expected)
+      context "given ids: #{input}" do
+        it "finds simularities '#{expected}'" do
+          expect(find_simularities(*input)).to eql expected
+        end
+      end
+    end
+
+    test_find_simularities(%w[abcde fghij], '')
+    test_find_simularities(%w[abcde klmno], '')
+    test_find_simularities(%w[abcde axcye], 'ace')
+    test_find_simularities(%w[fghij fguij], 'fgij')
+  end
+
+  describe '#find_common_chars' do
+    def self.test_common_chars(case_name, expected)
+      context "given input '#{case_name}'" do
+        let(:input) { with_input(case_name) }
+        subject(:results) { find_common_chars(input) }
+
+        it "finds common characters '#{expected}'" do
+          expect(results).to eql expected
+        end
+      end
+    end
+
+    test_common_chars('part-2-case-1.txt', 'fgij')
+    test_common_chars('input.txt', 'pbykrmjmizwhxlqnasfgtycdv')
   end
 end
 
